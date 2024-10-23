54897a96ae5aab575c3b6558a96e10c7210b48f3
宋秉徽 <451000936@qq.com>
2024-10-23 15:06:31 +0800

    test: :white_check_mark: test

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,5 @@
 export default {
   '**/*/*.{md,json}': ['npm run format --no-error-on-unmatched-pattern'],
-  '**/*/*.js': ['eslint "{src,apps,libs,test}/**/*.ts" --fix', 'npm run format'],
-  '**/*/*.ts': ['eslint "{src,apps,libs,test}/**/*.ts" --fix', 'npm run format --parser=typescript'],
+  '**/*/*.js': ['eslint --fix', 'npm run format'],
+  '**/*/*.ts': ['eslint --fix', 'npm run format --parser=typescript'],
 };
