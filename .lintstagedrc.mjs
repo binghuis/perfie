dfd88d5d7b4fd78490f474c2a31686dfb30bda86
宋秉徽 <451000936@qq.com>
2024-10-23 14:58:31 +0800

    refactor: :hammer: 重构

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,5 @@
 export default {
   '*.{md,json}': ['npm run format --no-error-on-unmatched-pattern'],
-  '*.{js}': ['npm run lint', 'npm run format'],
-  '*.{ts}': ['npm run lint', 'npm run format --parser=typescript'],
+  '*.js': ['npm run lint', 'npm run format'],
+  '*.ts': ['npm run lint', 'npm run format --parser=typescript'],
 };
