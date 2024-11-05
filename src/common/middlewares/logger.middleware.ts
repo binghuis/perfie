65c7cd3f83aea7a73f95159e069699d90713afcb
宋秉徽 <451000936@qq.com>
2024-11-05 14:59:11 +0800

    refactor: :hammer: monorepo

--- src/common/middlewares/logger.middleware.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export function logger(req: Request, res: Response, next: NextFunction) {
-  const { method, baseUrl } = req;
-
-  console.log(`${method} ${baseUrl}`);
-  next();
-}
